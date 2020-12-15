d64350125c6967423a5df0bc062fd7220394fbfe
DandyCV <dandycvc@gmail.com>
2020-12-15 16:30:45 +0200

    Add "Mailgun" smpt settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  validates :name, :surname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, :surname, presence: true
 
   enum role: %i(admin manager doctor patient)
 end
