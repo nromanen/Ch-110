666161622b3bc4ad45d831e30d52646b5adbc72f
DandyCV <dandycvc@gmail.com>
2021-01-13 16:45:34 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,43 @@ users = User.create([{email: 'admin@admin.com', password: 'admin1', password_con
                      {email: 'user1@gmail.com', password: '123456', password_confirmation: '123456', name: 'Tomy',
                       surname: 'Anjelo', phone_number: '380996775503', confirmed_at: '202012120000'},
                      {email: 'user2@gmail.com', password: '123456', password_confirmation: '123456', name: 'Jimm',
-                      surname: 'Klarck', phone_number: '380971225344', confirmed_at: '202012120000'}])
+                      surname: 'Klarck', phone_number: '380971225344', confirmed_at: '202012120000'},
+                     {email: 'manager@gmail.com', password: '123456', password_confirmation: '123456', name: 'Manager',
+                      surname: 'Manager', phone_number: '11111111111', role: 1, confirmed_at: '202012120000'}])
 
 visit_types = VisitType.create([{name: 'Inspection', description: 'Inspection', length: 30},
                                 {name: 'Consultation', description: 'Consultation', length: 20}])
+
+schedules = Schedule.create([{user_id: User.find_by(email: 'doctor1@gmail.com'), day: 'Monday', start_time: '2000-01-01 09:00:00',
+                              end_time: '2000-01-01 09:00:00', visit_type_id: 1, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor1@gmail.com'), day: 'Tuesday', start_time: '2000-01-01 14:00:00',
+                              end_time: '2000-01-01 18:00:00', visit_type_id: 1, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor1@gmail.com'), day: 'Wednesday', start_time: '2000-01-01 09:00:00',
+                              end_time: '2000-01-01 09:00:00', visit_type_id: 1, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor1@gmail.com'), day: 'Thursday', start_time: '2000-01-01 14:00:00',
+                              end_time: '2000-01-01 18:00:00', visit_type_id: 1, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor1@gmail.com'), day: 'Friday', start_time: '2000-01-01 09:00:00',
+                              end_time: '2000-01-01 09:00:00', visit_type_id: 1, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor2@gmail.com'), day: 'Monday', start_time: '2000-01-01 14:00:00',
+                              end_time: '2000-01-01 18:00:00', visit_type_id: 2, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor2@gmail.com'), day: 'Tuesday', start_time: '2000-01-01 09:00:00',
+                              end_time: '2000-01-01 13:00:00', visit_type_id: 2, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor2@gmail.com'), day: 'Wednesday', start_time: '2000-01-01 14:00:00',
+                              end_time: '2000-01-01 18:00:00', visit_type_id: 2, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor2@gmail.com'), day: 'Thursday', start_time: '2000-01-01 09:00:00',
+                              end_time: '2000-01-01 13:00:00', visit_type_id: 2, start_date: '2021-01-01', end_date: '2021-01-31'},
+                             {user_id: User.find_by(email: 'doctor2@gmail.com'), day: 'Friday', start_time: '2000-01-01 14:00:00',
+                              end_time: '2000-01-01 18:00:00', visit_type_id: 2, start_date: '2021-01-01', end_date: '2021-01-31'}])
+
+visits = Visit.create([{patient_id: User.find_by(email: 'user1@gmail.com'), doctor_id: User.find_by(email: 'doctor1@gmail.com'),
+                        start_time: '2021-01-08 10:00:00', visit_type: 1, created_by_id: User.find_by(email: 'user1@gmail.com')},
+                       {patient_id: User.find_by(email: 'user1@gmail.com'), doctor_id: User.find_by(email: 'doctor1@gmail.com'),
+                        start_time: '2021-01-13 11:00:00', visit_type: 1, created_by_id: User.find_by(email: 'user1@gmail.com')},
+                       {patient_id: User.find_by(email: 'user1@gmail.com'), doctor_id: User.find_by(email: 'doctor1@gmail.com'),
+                        start_time: '2021-01-19 15:00:00', visit_type: 1, created_by_id: User.find_by(email: 'user1@gmail.com')},
+                       {patient_id: User.find_by(email: 'user2@gmail.com'), doctor_id: User.find_by(email: 'doctor2@gmail.com'),
+                        start_time: '2021-01-12 12:00:00', visit_type: 2, created_by_id: User.find_by(email: 'user2@gmail.com')},
+                       {patient_id: User.find_by(email: 'user2@gmail.com'), doctor_id: User.find_by(email: 'doctor2@gmail.com'),
+                        start_time: '2021-01-15 17:00:00', visit_type: 2, created_by_id: User.find_by(email: 'user2@gmail.com')},
+                       {patient_id: User.find_by(email: 'user2@gmail.com'), doctor_id: User.find_by(email: 'doctor2@gmail.com'),
+                        start_time: '2021-01-18 16:00:00', visit_type: 2, created_by_id: User.find_by(email: 'user2@gmail.com')}])
